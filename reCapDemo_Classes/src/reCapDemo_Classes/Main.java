63b0a68e658170b5fa13c77b8bdafd276e4c2297
Emre SAGLAM <69041945+cukulaci@users.noreply.github.com>
2022-10-04 00:12:05 +0300

    Add files via upload

---
+++ reCapDemo_Classes/src/reCapDemo_Classes/Main.java
@@ -0,0 +1,14 @@
+package reCapDemo_Classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		DortIslem dortIslem = new DortIslem();
+		int sonuc = dortIslem.Toplama(5, 6);
+
+		System.out.println(sonuc);
+
+	}
+
+}
